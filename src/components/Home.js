63b4617a46d87ed46c9c73715d0f6f2184fbabf0
Mayank <mayank.nayak@hotmail.com>
2023-07-02 22:06:08 -0500

    removed unused stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,14 @@
-import { useState } from 'react';
 import Simple from './Simple.js';
-import Complex from './Complex.js';
+
 
 export default function Home() {
-	const [complex, setComplex] = useState(false);
+	
 
 
 
 
 	return (
-		complex === true ? <Complex /> : <Simple />
+		 <Simple />
 
 		
 
